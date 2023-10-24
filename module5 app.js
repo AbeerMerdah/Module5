50773e8e9a82a1f5239c120f2894f5cd64d22125
abeer <abeer@home.com>
2023-10-24 17:10:34 +0300

    five web file

---
+++ module5 app.js
@@ -0,0 +1,5 @@
+// Add any interactive functionality you want here
+// For example, handling clicks on the icons or menu items
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+});
